62e36704eda8dfc6574678ab1a98f1fdf88e4ff0
Fossilia <35693687+Fossilia@users.noreply.github.com>
2018-01-22 09:30:30 -0500

    Add files via upload

---
+++ Game--FROGGER/Frog.java
@@ -0,0 +1,30 @@
+public class Frog{
+    private int x = 298;
+    private int y = 460;
+    private int dir = 0;
+    
+    public void setDir(int d){
+        dir = d;
+    }
+    
+    public int getDir(){
+        return dir;
+    }
+    
+    public int getX(){
+        return x;
+    }
+    
+     public int getY(){
+        return y;
+    }
+     
+    public void moveX(int x){
+        this.x += x;
+    }
+    
+     public void moveY(int y){
+        this.y += y;
+    }
+    
+}
