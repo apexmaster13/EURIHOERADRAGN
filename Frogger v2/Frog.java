8c8b1b529466c4a6d52e39333520ec43ac369388
Fossilia <35693687+Fossilia@users.noreply.github.com>
2018-02-18 21:05:30 -0500

    Add files via upload

---
+++ Frogger v2/Frog.java
@@ -0,0 +1,101 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Frog
+{
+
+    private int x = 298;
+    private int y = 460;
+    private int dir = 0;
+    private int width = 24;
+    private int height = 24;
+    private Rectangle frogRect;
+    private Image frog, pic;
+    private final int UP = 0;
+    private final int DOWN = 1;
+    private final int RIGHT = 2;
+    private final int LEFT = 3;
+
+    public Rectangle getRect()
+    {
+        frogRect = new Rectangle(x,y, pic.getWidth(null), pic.getHeight(null));
+        return frogRect;
+    }
+
+    public void setPic(Image p)
+    {
+        pic = p;
+    }
+
+    public void setDir(int d)
+    {
+        dir = d;
+    }
+
+    public int getDir()
+    {
+        return dir;
+    }
+
+    public void update()
+    {
+        x = 298;
+        y = 460;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getHeight()
+    {
+        return height;
+    }
+
+    public int getWidth()
+    {
+        return width;
+    }
+
+     public int getY()
+     {
+        return y;
+    }
+
+    public void moveX(int x)
+    {
+        this.x += x;
+    }
+
+    public void moveY(int y)
+    {
+        this.y += y;
+    }
+
+    public void draw(Graphics g)
+    {
+        if(dir == UP){
+            //System.out.println("working");
+
+            g.drawImage(pic , x, y, null);
+        }
+        else if(dir == LEFT){
+
+            g.drawImage(pic , x, y, null);
+        }
+        else if(dir == DOWN){
+
+            g.drawImage(pic , x, y + 18, 24, -18, null);
+        }
+        else if(dir == RIGHT){
+
+            g.drawImage(pic , x + 18, y, -18, 24, null);
+
+        }
+        if(pic!= null){
+         //g.drawRect(x, y, pic.getWidth(null), pic.getHeight(null));
+        }
+    }
+
+}
---
+++ Frogger v2/Menu.java
@@ -0,0 +1,40 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+class Menu extends JFrame implements ActionListener{
+    private Frogger me3;
+    JButton playBtn = new JButton();
+    
+    
+    public Menu(Frogger m){
+        super ("Frogger");
+        setSize (466, 553);
+        me3 = m;
+        playBtn.addActionListener(this);
+        ImageIcon back = new ImageIcon("images/menus/menu.png");
+        //back = back.getScaledInstance(460, 524, Image.SCALE_SMOOTH);
+        JLabel backLabel = new JLabel(back);
+        JLayeredPane mPage=new JLayeredPane();
+        mPage.setLayout(null);
+        
+        backLabel.setSize(466, 524);
+        backLabel.setLocation(0,0);
+        mPage.add(backLabel,1);
+        
+        
+        //playBtn.setBackground(new Color(0,0,0));
+        playBtn.setOpaque(false);
+        playBtn.setSize(100,30);
+        playBtn.setLocation(190,270);
+        mPage.add(playBtn,2);
+        
+        add(mPage);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent evt) {
+        setVisible(false);
+        me3.start();
+    }
+}
