daa39cb2eeabb2548a8cda65f1a48ae5665534df
Fossilia <35693687+Fossilia@users.noreply.github.com>
2018-02-20 08:01:00 -0500

    Add files via upload

---
+++ Frogger v2/sounds/sound.java
@@ -0,0 +1,11 @@
+private static void playSound(String sound){
+    // cl is the ClassLoader for the current class, ie. CurrentClass.class.getClassLoader();
+    URL file = cl.getResource(sound);
+    final Media media = new Media(file.toString());
+    final MediaPlayer mediaPlayer = new MediaPlayer(media);
+    mediaPlayer.play();
+}
+
+static{
+    JFXPanel fxPanel = new JFXPanel();
+}
